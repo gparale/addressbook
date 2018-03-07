2bc2fc98d7a667e649d53c04316baaf64654e853
gparale <glennpparale@gmail.com>
2018-03-06 17:07:47 -0800

    uGHH

--- web_back_end.js
+++ web_back_end.js
@@ -2,6 +2,7 @@ const request = require('request')
 const express = require('express')
 const app = express()
 
+app.use(express.static(__dirname + "/src"))
 app.get("/",(request, response) => {
 	resp.sendFile(__dirname + "/web_front_end.html")
 	//response.end('This is a test for stuff')
