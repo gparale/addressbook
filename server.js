fc3f8a38b28124f6289119963417172ef751a42d
Rory Woo <rorycxnw@gmail.com>
2018-03-12 15:53:03 -0700

    FRONT END: ADDED LOG IN AND SIGN UP INPUTS AND BUTTONS

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const request = require('request')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(express.static(__dirname + "/src"))
 app.use(bodyParser.json()) //Needed for when retrieving JSON from front-end
 
 app.get("/",(request, response) => {
-	response.sendFile(__dirname + "/web_front_end.html")
+	response.sendFile(__dirname + "/front_end.html")
 	//response.end('This is a test for stuff')
 })
 
