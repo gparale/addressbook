398066b9fb56c73c992954ea4d888fa05794c281
gparale <glennpparale@gmail.com>
2018-04-03 16:35:29 -0700

    Started work on the hub

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//const request = require('request')
 const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
@@ -10,74 +9,70 @@ const app = express();
 var dbURL = process.env.DATABASE_URL || "postgres://localhost:5432/postgres"; // change this per db name
 
 app.use(express.static(__dirname + "/src"))
+
+
 app.use(bodyParser.json()) //Needed for when retrieving JSON from front-end
 app.set('view engine', 'hbs')
-app.use(session({secret: 'tolkien', saveUninitialized: false, resave: false, cookie: {maxAge: 5*60000}}))
+app.use(session({ secret: 'tolkien', saveUninitialized: false, resave: false, cookie: { maxAge: 5 * 60000 } }))
 
 
-app.get("/",(request, response) => {
+app.get("/", (request, response) => {
 
-	response.sendFile(__dirname + "/front_end.html")
-	//response.end('This is a test for stuff')
+    response.sendFile(__dirname + "/front_end.html")
+    //response.end('This is a test for stuff')
 })
 
-app.post("/resources", (request, response, next)=>{
-	/*if (request.body["name"] === "Glenn" && request.body["pass"] === "slit"){
-		request.session.myvar = request.body
-		console.log("Log From /resources")
-		console.log(request.session)
-		console.log(request.sessionID)
-		response.json({message: "Login Successful", url: "hub"})
-	} else{
-		response.json({message: "Login Failed", url: "Message Failed"})
-	}
-	*/
+app.post("/resources", (request, response, next) => {
+
     if (request.body["request-type"] === "login") {
-                        
-    }
-    else if  (request.body["request-type"] === "signup") {
-        
-    }
-	//response.redirect("/hub")
+        if (request.body["name"] === "glenn" && request.body["pass"] === "slit") {
+            request.session.myvar = request.body
+            response.json({ message: "Login Successful", url: "hub" })
+        } else {
+            response.json({ message: "Login Failed", url: "Message Failed" })
+        }
+    } /*else if (request.body["request-type"] === "signup") {
+
+    }*/
+    //response.redirect("/hub")
 })
 
-app.get("/hub", (request, response, next) =>{
-	console.log("log from /hub")
-
-	console.log(request.session)
-	console.log(request.sessionID);
-	
-	response.send(request.session.myvar)
+app.get("/hub", (request, response, next) => {
+	sessionInfos = request.session.myvar
+    response.render("hub.hbs", {
+    	username: sessionInfos["name"],
+    	sel: [{opt_name: "Profile", img_source: "https://d30y9cdsu7xlg0.cloudfront.net/png/138926-200.png"}, {opt_name: "Contacts", img_source: "http://www.gaby-moreno.com/administrator/public_html/css/ionicons/png/512/android-contacts.png"}]
+    })
 })
 
-app.post("/register", function(req, resp){
+app.post("/register", function(req, resp) {
     var passcode = req.body.thePasscode;
     var email = req.body.theEmail;
     var username = req.body.theUsername;
-    
-    pg.connect(dbURL, function(err, client, done){
-       if(err){
-           console.log(err);
-           var obj = {
-               status:"fail",
-               msg:"CONNECTION FAIL"
-           }
-           resp.send(obj);
+
+    pg.connect(dbURL, function(err, client, done) {
+        if (err) {
+            console.log(err);
+            var obj = {
+                status: "fail",
+                msg: "CONNECTION FAIL"
+            }
+            resp.send(obj);
         }
-        
-        client.query("INSERT INTO users (username, passcode, email) VALUES ($1, $2, $3)", [username, passcode, email], function(err, result){
+
+        client.query("INSERT INTO users (username, passcode, email) VALUES ($1, $2, $3)", [username, passcode, email], function(err, result) {
             done();
-            if(err){
+            if (err) {
                 console.log(err);
                 var obj = {
-                   status:"fail",
-                   msg:"Username/passcode/email is/are invalid"
+                    status: "fail",
+                    msg: "Username/passcode/email is/are invalid"
                 }
                 resp.send(obj);
             }
-                     
+
             var obj = {
-                status:"success"
+                status: "success"
             }
             resp.send(obj);
         });
@@ -85,9 +80,9 @@ app.post("/register", function(req, resp){
 });
 
 app.listen(3000, (err) => {
-	if (err) {
-		console.log('Server is down');
-		return false;
-	}
-	console.log('Server is up');
+    if (err) {
+        console.log('Server is down');
+        return false;
+    }
+    console.log('Server is up');
 })
