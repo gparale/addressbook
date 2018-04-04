ef190e474c2b7ff4685b8ec6ab8bce9bdffac172
gparale <glennpparale@gmail.com>
2018-04-04 15:24:54 -0700

    Figured out how to display other handlebars using uncanny methods

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const hbs = require('hbs')
+const fs = require('fs')
+
 const { Pool, Client } = require('pg')
 
 const app = express();
@@ -12,13 +14,21 @@ const pool = new Pool({
     connectionString: dbURL,
 })
 
-pool.query('SELECT username fROM USERS', (err, res) => {
+/*pool.query('SELECT username fROM USERS', (err, res) => {
     //console.log(err, res)
-    console.log(res.rows[0].username)
+    //console.log(res.rows[0].username)
     pool.end()
-})
+})*/
 
 app.use(express.static(__dirname + "/src"))
+
+app.use((request, response, next)=>{
+    profile = hbs.compile(fs.readFileSync(__dirname + "/views/radicals/profile.hbs", 'utf8'))
+    next();
+})
+
+
+hbs.registerPartials(__dirname + "/views/partials")
 
 
 app.use(bodyParser.json()) //Needed for when retrieving JSON from front-end
@@ -41,32 +51,50 @@ app.post("/resources", (request, response, next) => {
         } else {
             response.json({ message: "Login Failed", url: "Message Failed" })
         }
-    } /*else if (request.body["request-type"] === "signup") {
+    }
+    /*else if (request.body["request-type"] === "signup") {
 
-    }*/
+       }*/
     //response.redirect("/hub")
 })
 
 app.get("/hub", (request, response, next) => {
-	sessionInfos = request.session.myvar
+    sessionInfos = request.session.myvar
+
     response.render("hub.hbs", {
-    	username: sessionInfos["name"],
-    	sel: [{opt_name: "Profile", img_source: "https://d30y9cdsu7xlg0.cloudfront.net/png/138926-200.png"}, {opt_name: "Contacts", img_source: "http://www.gaby-moreno.com/administrator/public_html/css/ionicons/png/512/android-contacts.png"}]
+        username: sessionInfos["name"],
+        sel: [{
+            opt_name: "Profile",
+            img_source: "https://d30y9cdsu7xlg0.cloudfront.net/png/138926-200.png",
+            layout: profile({
+                fname: "Romy",
+                lname: "Li",
+                comment: "He's Romy Li"
+            })
+        }, {
+            opt_name: "Contacts",
+            img_source: "http://www.gaby-moreno.com/administrator/public_html/css/ionicons/png/512/android-contacts.png",
+            layout: profile({
+                fname: "Romy",
+                lname: "Li",
+                comment: "He's Romy Li"
+            })
+        }]
     })
 })
 
-app.post("/signup", function(req, resp){
+app.post("/signup", function(req, resp) {
     var username = req.body.name;
     var passcode = req.body.pass;
-    
-    pg.connect(dbURL, function(err, client, done){
-       if(err){
-           console.log(err);
-           var obj = {
-               status:"fail",
-               msg:"CONNECTION FAIL"
-           }
-           resp.send(obj);
+
+    pg.connect(dbURL, function(err, client, done) {
+        if (err) {
+            console.log(err);
+            var obj = {
+                status: "fail",
+                msg: "CONNECTION FAIL"
+            }
+            resp.send(obj);
         }
     })
 });
