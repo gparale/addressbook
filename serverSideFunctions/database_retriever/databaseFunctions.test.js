346a951bc809fb55a4b29c9088bf65198e5b3d10
SamHadavi <s.hadavi@gmail.com>
2018-05-18 16:26:37 -0700

    more test

--- serverSideFunctions/database_retriever/databaseFunctions.test.js
+++ serverSideFunctions/database_retriever/databaseFunctions.test.js
@@ -19,13 +19,60 @@ test('test user data', () => {
 
 test('test contact account by first and last name', () => {
 	databaseFun.getContAccount('Thaman', 'Woo').then((result) => {
-		expect(result).toMatchObject([ { user_id: 1, fname: 'Thaman', lname: 'Woo' } ])
+		expect(result).toMatchObject([{user_id:1,fname:'Thaman',lname:'Woo'}])
 	})
 })
 
 test('test addresses of a contact', () => {
 	databaseFun.getContactAddresses(1, 2).then((result) => {
-		expect(result).toMatchObject([ '3766 E 1st Ave, Burnaby', '3766 E 1st Ave, Burnaby' ])
+		expect(result).toMatchObject(['3766 E 1st Ave, Burnaby','3766 E 1st Ave, Burnaby'])
 	})
 })
 
+test('test phone of a contact', () => {
+	databaseFun.getContactPhone(1, 2).then((result) => {
+		expect(result).toMatchObject(['1232341232'])
+	})
+})
+
+test('test contacts of a user', () => {
+	databaseFun.getContactsInfo(1).then((result) => {
+		expect(result).toMatchObject(
+			[ { cont_id: 1,
+		    firstname: 'Li',
+		    lastname: 'Pho',
+		    with_account: false,
+		    acct_num: null },
+		  { cont_id: 2,
+		    firstname: 'Sam',
+		    lastname: 'Pho',
+		    with_account: false,
+		    acct_num: null },
+		  { cont_id: 3,
+		    firstname: 'Turd',
+		    lastname: 'Master',
+		    with_account: true,
+		    acct_num: 2 } ])
+	})
+})
+
+test('test contacts of a user alt', () => {
+	databaseFun.getContInfo(1).then((result) => {
+		expect(result).toMatchObject(
+			[ { cont_id: 1,
+		    fname: 'Li',
+		    lname: 'Pho',
+		    addresses: [ '555 Seymour Street' ],
+		    phone_numbers: [ '6044491231', '7341230987' ] },
+		  { cont_id: 2,
+		    fname: 'Sam',
+		    lname: 'Pho',
+		    addresses: [ '3766 E 1st Ave, Burnaby', '3766 E 1st Ave, Burnaby' ],
+		    phone_numbers: [ '1232341232' ] },
+		  { cont_id: 3,
+		    fname: 'Turd',
+		    lname: 'Master',
+		    addresses: [ 'Scott Road Station, 110th Ave, Surrey, BC' ],
+		    phone_numbers: [ '6341234235' ] } ])
+	})
+})
